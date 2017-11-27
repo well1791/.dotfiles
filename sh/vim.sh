6155a89f768c35ba7a9f7edab89fdf38a072d285
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-11-27 13:39:32 -0500

    Remove vim-split from te alias

--- sh/vim.sh
+++ sh/vim.sh
@@ -9,5 +9,5 @@ is_file "${ZSHRC}" && alias ze="vim ${ZSHRC} && source ${ZSHRC}"
 TMUX_CONF="${HOME}/.tmux.conf"
 TMUX_LOCAL="${HOME}/.tmux.conf.local"
 if is_file "$TMUX_CONF" && is_file "$TMUX_LOCAL"; then
-  alias te="vim -O $TMUX_LOCAL $TMUX_CONF && tmux source ${TMUX_CONF} && tmux display '${TMUX_CONF} sourced'"
+  alias te="vim $TMUX_LOCAL $TMUX_CONF && tmux source ${TMUX_CONF} && tmux display '${TMUX_CONF} sourced'"
 fi
