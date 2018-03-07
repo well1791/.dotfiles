6b1d7839cf277abd75f85e78f6d6a00718621dde
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2018-03-07 13:47:44 -0500

    Add stack aliases

--- sh/hs.sh
+++ sh/hs.sh
@@ -1,9 +1,12 @@
 ! is_cmd "stack" && return 0
 
-alias sb="stack build"
+alias sb="stack build --file-watch --fast --pedantic"
 alias se="stack exec"
 alias si="stack install"
-alias sr="stack repl"
+alias sr="stack repl --test"
 alias st="stack test"
+
+alias yd="dotenv -f .env 'stack exec -- yesod devel'"
+
 alias ghci="stack exec -- ghci"
 alias ghcmod="ghc-mod"
