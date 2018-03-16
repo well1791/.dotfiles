3e8183421f67ca1815ed9d3ee1a26ae575f8253d
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2018-03-16 17:10:31 -0500

    Improve haskell aliases

--- sh/hs.sh
+++ sh/hs.sh
@@ -1,12 +1,12 @@
 ! is_cmd "stack" && return 0
 
-alias sb="stack build --file-watch --fast --pedantic"
+alias yd="dotenv -f .env 'stack exec -- yesod devel'"
+
+alias sb="dotenv -f .env.test 'stack build --file-watch --fast --pedantic'"
+alias sr="dotenv -f .env.test 'stack repl --test'"
+alias st="dotenv -f .env.test 'stack test'"
 alias se="stack exec"
 alias si="stack install"
-alias sr="stack repl --test"
-alias st="stack test"
-
-alias yd="dotenv -f .env 'stack exec -- yesod devel'"
 
 alias ghci="stack exec -- ghci"
 alias ghcmod="ghc-mod"
