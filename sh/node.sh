aa52842dc5b097bcb497f592ba0ea10d6863d2c8
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2018-10-20 12:42:00 -0500

    Improve nvm loading

--- sh/node.sh
+++ sh/node.sh
@@ -1,6 +1,23 @@
-export_var "NVM_DIR" "${HOME}/.nvm" is_dir
-
 export_path "${HOME}/.yarn/bin"
 
-src_file "${NVM_DIR}/nvm.sh"
-src_file "${NVM_DIR}/bash_completion"
+# source: http://broken-by.me/lazy-load-nvm/
+nvm() {
+    unset -f nvm
+    export_var "NVM_DIR" "${HOME}/.nvm" is_dir
+    src_file "${NVM_DIR}/nvm.sh"
+    nvm "$@"
+}
+
+node() {
+    unset -f node
+    export_var "NVM_DIR" "${HOME}/.nvm" is_dir
+    src_file "${NVM_DIR}/nvm.sh"
+    node "$@"
+}
+
+npm() {
+    unset -f npm
+    export_var "NVM_DIR" "${HOME}/.nvm" is_dir
+    src_file "${NVM_DIR}/nvm.sh"
+    npm "$@"
+}
