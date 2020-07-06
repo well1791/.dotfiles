805844298a5fdf0b64ab3b02a93f6974cdcab1aa
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2020-07-06 13:40:29 -0400

    Update Spacevim confs

--- sh/vim.sh
+++ sh/vim.sh
@@ -1,13 +1,22 @@
-! is_cmd "vim" && return 0
+if is_cmd "nvim"; then
+  v="nvim"
+elif is_cmd "vim"; then
+  v="vim"
+fi
 
-export VISUAL=vim
-export EDITOR="${VISUAL}"
+! is_cmd "$v" && return 0
+
+alias v="$v"
+export VISUAL="$v"
+export EDITOR="$v"
+
+alias vimdiff="$v -d"
 
 ZSHRC="${HOME}/.zshrc"
-is_file "${ZSHRC}" && alias ze="vim ${ZSHRC} && source ${ZSHRC}"
+is_file "${ZSHRC}" && alias ze="$v $ZSHRC && source $ZSHRC"
 
 TMUX_CONF="${HOME}/.tmux.conf"
 TMUX_LOCAL="${HOME}/.tmux.conf.local"
 if is_file "$TMUX_CONF" && is_file "$TMUX_LOCAL"; then
-  alias te="vim $TMUX_LOCAL $TMUX_CONF && tmux source ${TMUX_CONF} && tmux display '${TMUX_CONF} sourced'"
+  alias te="$v $TMUX_LOCAL $TMUX_CONF && tmux source $TMUX_CONF && tmux display '${TMUX_CONF} sourced'"
 fi
